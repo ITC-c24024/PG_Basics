5464492aee528111a1edc0bff41dbba4c2bba282
ITC-c24024 <c24024@std.it-college.ac.jp>
2025-05-20 14:48:42 +0900

    P48 第3章 チャレンジ問題 終了

---
+++ Chapter03/challenge3-03.py
@@ -0,0 +1,8 @@
+i=20
+
+if i<10:
+    print("iは10以下")
+elif i>10 and i<=25:
+    print("iは10より大きく、25より小さい")
+elif i>25:
+    print("iは25より大きい")
---
+++ Chapter03/challenge3-06.py
@@ -0,0 +1,6 @@
+age=19
+
+if age<18:
+    print("未成年")
+elif age >=18:
+    print("成人")
