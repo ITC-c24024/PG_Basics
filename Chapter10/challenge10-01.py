3af5a135c8ceaa5117612518f1c9be5dc45af84a
ITC-c24024 <c24024@std.it-college.ac.jp>
2025-06-17 16:19:08 +0900

    P139 第10章 チャレンジ問題 終了

---
+++ Chapter10/challenge10-01.py
@@ -0,0 +1,36 @@
+print("ハングマンへようこそ！")
+import random
+def hangman(word):
+    wrong=0
+    stages=["",
+            "_______      ",
+            "|     |      ",
+            "|     o      ",
+            "|    /|\\    ",
+            "|    / \\    ",
+            "|            "
+            ]
+    rletters=list(word)
+    board=["__"]*len(word)
+
+    while wrong <len(stages)-1:
+        print(" ".join(board))
+        char=input("1文字を予想してね")
+
+        if char in rletters:
+            cind=rletters.index(char)
+            board[cind]=char
+            rletters[cind]="$"
+        else:
+            wrong+=1
+        print(" ".join(board))
+        print("\n".join(stages[0:wrong+1]))
+        if "__" not in board:
+            print("あなたの勝ち！")
+            break
+        if wrong==len(stages)-1:
+            print(f"あなたの負け！正解は{word}")
+
+wordList=["apple","banana","cat","dog","egg","fire","gravity","hand","ice","juice"]
+
+hangman(random.choice(wordList))
